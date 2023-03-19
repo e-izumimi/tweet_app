57427f2c4ab92103d6573fd3b98e36bf273cad92
izumimi <e.izumimi@gmail.com>
2023-03-19 13:05:30 +0900

    0319commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "posts/index" => "posts#index"
+  get "posts/:id" => "posts#show"
   get "/" => "home#top"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
